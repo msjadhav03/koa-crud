82d13fb78ab387711836b5b5174c3c7144adfe7f
msjadhav03 <manisha.jadhav2323@gmail.com>
2023-08-09 21:52:51 +0530

    basic routing

--- REDAME.md
+++ REDAME.md
@@ -1,7 +1,95 @@
-#KOA
+# KOA
 - Web Application Framework
 - Faster than express
 - Designed by Express team
 - Efficient to handle more request than Express
 
-##
+## Installing Koa
+```
+npm i --save koa
+```
+## Initalizing Koa Package
+```
+npm init -y
+```
+
+## Simple Koa Server
+```
+const Koa = require('koa);
+const app = new Koa()
+const PORT = 4000
+app.listen(PORT)
+console.log(`Server is running on ${PORT}`)
+```
+
+## Routing
+
+### Installing router package
+```
+npm i --save koa-router
+```
+### Routing for GET Method
+
+```
+const Koa = require('koa);
+const app = new Koa()
+const PORT = 4000
+const Router = require('koa-router')
+const router = new Router()
+
+router.get('/',ctx =>{
+    ctx.body = "Welcome to Koa App"
+})
+
+app.listen(PORT)
+console.log(`Server is running on ${PORT}`)
+```
+
+
+### Routing for GET BY id Method
+
+```
+const Koa = require('koa);
+const app = new Koa()
+const PORT = 4000
+const Router = require('koa-router')
+const router = new Router()
+const data = [
+    {
+        "id" : "1",
+        "breed" : "Siberian Husky"
+    }
+]
+router.get('/:id',ctx =>{
+    ctx.body = data.find( element => element.id === ctx.request.params)
+})
+
+app.listen(PORT)
+console.log(`Server is running on ${PORT}`)
+```
+
+
+### Routing for DELETE BY id Method
+
+```
+const Koa = require('koa);
+const app = new Koa()
+const PORT = 4000
+const Router = require('koa-router')
+const router = new Router()
+const data = [
+    {
+        "id" : "1",
+        "breed" : "Siberian Husky"
+    }
+]
+router.delete('/:id',ctx =>{
+    const element = data.find( element => element.id === ctx.request.params)
+    data.remove(element)
+    ctx.status = 200
+    ctx.body = element
+})
+
+app.listen(PORT)
+console.log(`Server is running on ${PORT}`)
+```
--- server.js
+++ server.js
@@ -1,15 +1,91 @@
 const Koa = require('koa');
 const app = new Koa()
-const PORT = 3000
+const PORT = 4000
+const Router = require('koa-router')
+const bodyparser = require('koa-parser')
 
+// Creating instance of Router
+const router = Router()
 
+// Using Body parser 
+app.use(bodyparser())
+
+// Custom Middleware
 app.use(async (ctx,next)=>{
     console.log(`Request Method : ${ctx.method} \n Request URL: ${ctx.url}`)
    return next()
 })
 
-app.use(async ctx =>{
-    ctx.body = "Koa APP"
+// Dummy Data
+const data = [
+    {
+        "id":"1",
+        "breed": "Siberian Husky",
+        "name":"Groot"
+    },
+    {
+        "id":"2",
+        "breed":"Pom",
+        "name": "loaf"
+    },
+    {
+        "id":"3",
+        "breed": "Golden Reteriver",
+        "name": "Oscar"
+    }
+]
+
+
+// GET Route
+router.get('/pets',async (context) =>{
+    console.log(context.url)
+    context.status = 200
+    context.body = data
 })
+
+// GET BY ID Route
+router.get('/pets/:id',async (context) =>{
+    console.log('',context.url)
+    context.body = data.find(element => element.id === context.params.id)
+    context.status = 200
+})
+
+//DELETE BY ID route
+router.delete('/pets/:id',async (context) =>{
+    console.log('',context.url)
+    const index = data.findIndex(element => element.id === context.params.id)
+    const element = data[index]
+    data.splice(index,1)
+    context.body = data
+})
+
+//Update BY ID route
+router.patch('/pets/:id',async (context) =>{
+    console.log('',context.url, context.request.body)
+    const index = data.findIndex(element => element.id === context.params.id)
+    if(context.request.body?.name || context.request.body?.breed)
+    {
+        data[index].name = context.request.body?.name || data[index].name
+        data[index].breed = context.request.body?.breed || data[index].breed
+    }
+    context.body = data
+})
+
+router.post('/pets',async (context)=>{
+    if(context.request.body == null)
+    {
+        context.throw(400,'Bad Request')
+    }
+    data.push(context.request.body)
+    context.body = data
+})
+
+app.on('error', err => {
+  console.error('server error', err)
+});
+
+// Registering routes with Application
+app.use(router.routes())
+
 app.listen(PORT)
 console.log('Server is running on port',PORT)
